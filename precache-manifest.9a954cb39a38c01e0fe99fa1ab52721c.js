9ecd0b13256306b083daf12de650dc8065c0a490
JawadRafique <jawadrafique07@gmail.com>
2020-10-13 13:18:16 +0500

    Updates

---
+++ precache-manifest.9a954cb39a38c01e0fe99fa1ab52721c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2cd76ad85075cd3f3996ce6cc0c15ab",
+    "url": "/LandingPage-React/index.html"
+  },
+  {
+    "revision": "e9645f9aa5510a0bbf77",
+    "url": "/LandingPage-React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "355442195355c0985cef",
+    "url": "/LandingPage-React/static/js/2.877693c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LandingPage-React/static/js/2.877693c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9645f9aa5510a0bbf77",
+    "url": "/LandingPage-React/static/js/main.1dae132f.chunk.js"
+  },
+  {
+    "revision": "655d618bb5347ccbc499",
+    "url": "/LandingPage-React/static/js/runtime-main.cacaeec2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LandingPage-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
